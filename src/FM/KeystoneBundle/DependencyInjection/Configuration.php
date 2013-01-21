ed5c730d158d0e53dd40ed9f4b01c13757fa382a
fieg <jeroen@financial-media.nl>
2013-01-21 15:06:42 +0100

    made user class and token class configurable

--- src/FM/KeystoneBundle/DependencyInjection/Configuration.php
+++ src/FM/KeystoneBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,12 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('fm_keystone');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->scalarNode('user_class')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('token_class')->isRequired()->cannotBeEmpty()->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
--- src/FM/KeystoneBundle/DependencyInjection/FMKeystoneExtension.php
+++ src/FM/KeystoneBundle/DependencyInjection/FMKeystoneExtension.php
@@ -25,5 +25,42 @@ class FMKeystoneExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('security.yml');
+
+        $this->remapParametersNamespaces($config, $container, array(
+            '' => array(
+                'user_class'  => 'fm_keystone.model.user.class',
+                'token_class' => 'fm_keystone.model.token.class',
+            ),
+        ));
+    }
+
+    protected function remapParameters(array $config, ContainerBuilder $container, array $map)
+    {
+        foreach ($map as $name => $paramName) {
+            if (array_key_exists($name, $config)) {
+                $container->setParameter($paramName, $config[$name]);
+            }
+        }
+    }
+
+    protected function remapParametersNamespaces(array $config, ContainerBuilder $container, array $namespaces)
+    {
+        foreach ($namespaces as $ns => $map) {
+            if ($ns) {
+                if (!array_key_exists($ns, $config)) {
+                    continue;
+                }
+                $namespaceConfig = $config[$ns];
+            } else {
+                $namespaceConfig = $config;
+            }
+            if (is_array($map)) {
+                $this->remapParameters($namespaceConfig, $container, $map);
+            } else {
+                foreach ($namespaceConfig as $name => $value) {
+                    $container->setParameter(sprintf($map, $name), $value);
+                }
+            }
+        }
     }
 }
