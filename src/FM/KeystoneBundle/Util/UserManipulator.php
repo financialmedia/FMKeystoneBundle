bd072556f192f8d8d153fcc54f2099cc565bed8f
Peter Kruithof <pkruithof@gmail.com>
2013-03-19 21:06:10 +0100

    Fixed create method in UserManipulator

--- src/FM/KeystoneBundle/Util/UserManipulator.php
+++ src/FM/KeystoneBundle/Util/UserManipulator.php
@@ -30,13 +30,9 @@ class UserManipulator
      */
     public function create($username, $password, $email, $active)
     {
-        $user = $this->userManager->createUser();
-
-        $user->setUsername($username);
+        $user = $this->userManager->createUser($username, $password, array());
         $user->setEmail($email);
-        $user->setPlainPassword($password);
         $user->setEnabled((boolean) $active);
-        $user->setRoles(array());
 
         $this->userManager->updateUser($user);
 
