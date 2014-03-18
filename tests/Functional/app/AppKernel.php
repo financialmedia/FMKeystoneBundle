77ff66c3217cbb6f6f6d84f4ec45deb7a859ee5a
ben <benniekrijger@hotmail.com>
2014-03-18 09:36:05 +0100

    removed FrameworkExtraBundle dependency

--- tests/Functional/app/AppKernel.php
+++ tests/Functional/app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
             new \Symfony\Bundle\MonologBundle\MonologBundle(),
-            new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new \FM\KeystoneBundle\FMKeystoneBundle(),
             new \FM\KeystoneIntegrationBundle\FMKeystoneIntegrationBundle()
